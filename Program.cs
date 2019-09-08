88e931bb2006389bbaeadb72a6aa46e19af986f3
Dylan <Dylan@LAPTOP-HU97QSV9>
2019-09-08 15:42:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IterativeStatementDeliverable_Stewart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Integer between 1 and 100 to execute an iterative statement");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                if ((value > 0) && (value < 101))
+                {
+                    Console.WriteLine("Executing a For Loop!");
+                    Console.WriteLine("The For Loop will iterate " + value.ToString() + " times.");
+
+                    for (int i = 0; i < value; i++)
+                    {
+                        Console.WriteLine("This is the current integer value in the loop: " + i.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program ...");
+                    // Pause the program and await the user to press a key to end the program
+                    Console.ReadKey(true);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter an integer value within the stated values and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program, thank you for using it!...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
